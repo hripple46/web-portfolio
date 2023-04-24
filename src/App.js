f87fc335f4ee8e53b90b3d4907d4d1b98a48d6d5
Henry Ripple <hripple46@gmail.com>
2023-04-24 16:07:52 -0500

    added profile pic and basic colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import ProfilePic from "./img/IMG_0040_Export_2.png";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="bg-gray-800 h-full">
+      <div className="h-screen">
+        <nav className="fixed top-0 left-0 right-0 flex items-center justify-between  bg-gray-500 h-16"></nav>
+        <div className="h-screen">
+          <img className="h-1/2 w-auto mt-16" src={ProfilePic} alt="Profile" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
