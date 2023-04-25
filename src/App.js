595f7992a439087bb7e63349a3abe87550e77d6f
Henry Ripple <hripple46@gmail.com>
2023-04-24 20:12:43 -0500

    added more styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
-import ProfilePic from "./img/IMG_0040_Export_3.png";
+import ProfilePic from "./img/IMG_0040_Export_4.png";
 
 function App() {
   return (
     <div className="bg-gray-800 h-full">
       <nav className="fixed top-0 left-0 right-0 flex items-center justify-between  bg-gray-500 h-16"></nav>
       <div className="h-screen md-16 border-b-4 flex items-center justify-evenly">
-        <img className="h-1/2 w-auto mt-16" src={ProfilePic} alt="Profile" />
+        <div className="h-3/5 relative">
+          <img className="h-full w-auto" src={ProfilePic} alt="Profile" />
+          <div className="absolute h-16 w-32 bg-blue-400 rounded-md top-0 -left-8 animate-bounce"></div>
+          <div className="absolute h-16 w-32 bg-blue-400 rounded-md bottom-0 -left-8 animate-bounce"></div>
+          <div className="absolute h-16 w-32 bg-blue-400 rounded-md  -right-8 top-2/4 animate-bounce"></div>
+        </div>
         <div>
           <h1 className="text-4xl text-white">Hi, I'm Henry</h1>
         </div>
