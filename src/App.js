4bc106b868cab56ef536d490cb55938a12145f9c
Henry Ripple <hripple46@gmail.com>
2023-04-25 14:30:12 -0500

    added minor styling fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import ProfilePic from "./img/IMG_0040_Blue.png";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FloatingSkills from "./FloatingSkills";
 import Projects from "./Projects";
 
 function App() {
+  useEffect(() => {
+    // This is to prevent the page from scrolling to the bottom when the page is refreshed
+    window.history.scrollRestoration = "manual";
+  }, []);
+
   return (
     <div className="bg-indigo-950 h-full">
       <nav className="fixed top-0 left-0 right-0 flex items-center justify-between   h-16">
@@ -15,7 +20,7 @@ function App() {
         </ul>
       </nav>
       <div className="h-screen md-16 border-b-4 flex items-center justify-evenly">
-        <div className="h-3/5 relative">
+        <div className="h-3/5 relative w-[360px] ">
           <img className="h-full w-auto" src={ProfilePic} alt="Profile" />
           <FloatingSkills
             style={{ top: "0", left: "-2rem" }}
@@ -42,7 +47,24 @@ function App() {
         </div>
       </div>
       <div className="relative h-screen">
-        <Projects />
+        <Projects
+          xPosition={"left-0"}
+          scrollPosition={200}
+          newPosition={"top-0"}
+          oldPosition={"top-full"}
+        />
+        <Projects
+          xPosition={"right-1/3"}
+          scrollPosition={400}
+          newPosition={"top-48"}
+          oldPosition={"top-full"}
+        />
+        <Projects
+          xPosition={"right-0"}
+          scrollPosition={500}
+          newPosition={"top-96"}
+          oldPosition={"top-full"}
+        />
       </div>
     </div>
   );
--- src/Projects.js
+++ src/Projects.js
@@ -3,7 +3,8 @@ import "./Projects.css";
 const Projects = (props) => {
   const [isVisible, setIsVisible] = useState(false);
   const getScrollPosition = () => {
-    if (window.scrollY > 200) {
+    console.log(window.scrollY);
+    if (window.scrollY > props.scrollPosition) {
       setIsVisible(true);
     }
   };
@@ -17,8 +18,11 @@ const Projects = (props) => {
 
   return (
     <div
-      className={`absolute  transition-all ease-in delay-300 h-1/4 w-1/3 bg-sky-400 rounded-md ${
-        isVisible ? `top-0` : `top-full`
+      className={`absolute ${
+        props.xPosition
+      }  transition-all ease-in delay-300 h-1/4 w-1/3 bg-sky-400 rounded-md ${
+        isVisible ? props.newPosition : props.oldPosition
+      }
       }`}
     ></div>
   );
