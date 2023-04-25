012239e82aee8f87149d62e58954c3575f3acfbf
Henry Ripple <hripple46@gmail.com>
2023-04-24 19:39:43 -0500

    added flex layout and made profile page 100vh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
-import ProfilePic from "./img/IMG_0040_Export_2.png";
+import ProfilePic from "./img/IMG_0040_Export_3.png";
 
 function App() {
   return (
     <div className="bg-gray-800 h-full">
-      <div className="h-screen">
-        <nav className="fixed top-0 left-0 right-0 flex items-center justify-between  bg-gray-500 h-16"></nav>
-        <div className="h-screen">
-          <img className="h-1/2 w-auto mt-16" src={ProfilePic} alt="Profile" />
+      <nav className="fixed top-0 left-0 right-0 flex items-center justify-between  bg-gray-500 h-16"></nav>
+      <div className="h-screen md-16 border-b-4 flex items-center justify-evenly">
+        <img className="h-1/2 w-auto mt-16" src={ProfilePic} alt="Profile" />
+        <div>
+          <h1 className="text-4xl text-white">Hi, I'm Henry</h1>
         </div>
       </div>
     </div>
