ee0bc1272495591f9f11966c9631941d9b041975
Henry Ripple <hripple46@gmail.com>
2023-04-27 16:22:22 -0500

    mild styling to skills

--- src/PopupSkills.js
+++ src/PopupSkills.js
@@ -1,10 +1,12 @@
 import React from "react";
 const PopupSkills = (props) => {
   return (
-    <div className="bg-white w-20">
-      <p className="text-white">{props.title}</p>
+    <div className="m-2 relative bg-white w-20 h-20 flex justify-center items-center">
+      <p className="mb-2 text-black absolute bottom-0 z-0">{props.title}</p>
       {props.image}
     </div>
   );
 };
 export default PopupSkills;
+
+//To-do: add a hover effect to the skills where svg translates up and the title appears
